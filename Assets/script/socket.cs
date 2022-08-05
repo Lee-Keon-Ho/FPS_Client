1c7b191f7afdc83fac37c2e67364160fc0b45e53
Lee-Keon-Ho <55432783+Lee-Keon-Ho@users.noreply.github.com>
2022-08-05 23:52:36 +0900

    update

---
+++ Assets/script/socket.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class socket : MonoBehaviour
+{
+    byte[] buffer;
+    MemoryStream memoryStream;
+    BinaryWriter binaryWriter;
+    Socket m_socket;
+    void Awake()
+    {
+        buffer = new byte[65535];
+        memoryStream = new MemoryStream(buffer);
+        binaryWriter = new BinaryWriter(memoryStream);
+
+        m_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("59.30.46.242"), 30002);
+
+            m_socket.Connect(iPEndPoint);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoginButton(TextMeshProUGUI _textMesh)
+    {
+        byte[] str = System.Text.Encoding.Unicode.GetBytes(_textMesh.text);
+        binaryWriter.Write((ushort)(sizeof(int) + str.Length));
+        binaryWriter.Write((ushort)2);
+        binaryWriter.Write(str);
+
+        int size = m_socket.Send(buffer, (int)memoryStream.Position, 0);
+
+        Debug.Log("str size" + str.Length);
+
+        for(int i = 0; i < str.Length; i ++)
+        {
+            Debug.Log(str[i]);
+
+        }
+        memoryStream.Position = 0;
+
+        if (size > 0) SceneManager.LoadScene("Lobby");
+    }
+
+    private void OnDestroy()
+    {
+        // Á¾·á ¹× logout packet
+    }
+}
